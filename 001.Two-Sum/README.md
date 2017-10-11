434eabb7ee872b0f56408b578b7d64800b9dd1f8
tsingwong <919916459@qq.com>
2017-10-11 22:10:45 +0800

    update 001 js other method

--- 001.Two-Sum/README.md
+++ 001.Two-Sum/README.md
@@ -6,6 +6,14 @@ You may assume that each input would have **exactly** one solution, and you may 
 
 ### 解析
 
-- First of all, we should judge the `length` of `nums`,if the `length` less than 1，return `false`
+- First of all, we should judge the `length` of `nums`,if the `length` less than 1，return `[]`
 - Use Array `other_nums` to save already looped number
 - if `target` minus loop value in the Array `other_nums`, return the Array contain result and index. Else send loop value to `other_nums`.
+- The Last, when there are no result , we should return `[]`
+
+### 解析2
+
+- First of all, we should judge the `length` of `nums`,if the `length` less than 1，return `false`
+- Use `k` to count, loop the value of `nums`, make other Array `temp_nums` equal to slice `nums` from `k` to the end
+- If `target` minus loop value in `temp_nums`, return the Array contain `k - 1` and `nums.indexOf(j)`
+- The Last, when there are no result , we should return `[]`
--- 001.Two-Sum/two-Sum.js
+++ 001.Two-Sum/two-Sum.js
@@ -2,12 +2,12 @@
  * @Author: tsingwong 
  * @Date: 2017-10-11 18:15:50 
  * @Last Modified by: tsingwong
- * @Last Modified time: 2017-10-11 19:35:50
+ * @Last Modified time: 2017-10-11 21:56:51
  */
-var twoSum = function(nums, target) {
-    let len = nums.length;
+var twoSum = function (nums, target) {
+    const len = nums.length;
     if (len <= 1) {
-        return false;
+        return [];
     }
     let other_nums = [];
     for (let [index, val] of nums.entries()) {
@@ -18,4 +18,24 @@ var twoSum = function(nums, target) {
             other_nums[index] = val;
         }
     }
+    return [];
 };
+
+let twoSum2 = function (nums, target) {
+    const len = nums.length;
+    if (len <= 1) {
+        return [];
+    }
+    let k = 0;
+    for (let val of nums) {
+        let j = target - val;
+        k += 1;
+        let temp_nums = nums.slice(k);
+        if (temp_nums.indexOf(j) > -1) {
+            return [k - 1, nums.indexOf(j)];
+        }
+    }
+    return [];
+}
+
+console.log(twoSum([1, 9, 7, 3, 7], 12));
